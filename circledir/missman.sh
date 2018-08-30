745bc16711dc3631c24717b98292747a2d56253e
Cormac O'Riordan <cormac@circleci.com>
2018-08-30 21:03:57 +0100

    CCI-ify

--- circledir/missman.sh
+++ circledir/missman.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
+if [[ $1 ]]
+then
 repo="$1"
+else
+repo="${repo}"
+fi
+
 token="$(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:circleci/"${repo}":pull" | jq --raw-output .token)" 
-#list="$(curl -s -H "Authorization: Bearer "${token}"" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" "https://registry-1.docker.io/v2/circleci/php/tags/list" | sed 's/.*\[//' | sed 's/\]\"\}//' | tr -d '"')"
-#cols="$(awk -F',' '{print NF}' <<< "$list")"
 list="$(wget -q https://registry.hub.docker.com/v1/repositories/circleci/"${repo}"/tags -O -  | sed -e 's/[][]//g' -e 's/"//g' -e 's/ //g' | tr '}' '\n'  | awk -F: '{print $3}')"
 lines="$(wc -l <<< "$list")"
 echo LINES=$lines
@@ -11,13 +16,12 @@ for ((i=1 ; i<=$lines ; i++))
 do
 tag="$(head -n$i <<< "$list" | tail -1)"
 echo trying tag $tag
-response="$(curl -iI -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/circleci/php/manifests/"${tag}"")"
+response="$(curl -siI -H "Authorization: Bearer $token" "https://registry-1.docker.io/v2/circleci/php/manifests/"${tag}"")"
 response_stripped="$(echo "$response" | tr '\r' ' ' | tr '\n' ' ' | sed 's/ \{3,\}/ /g' | sed 's/   / /g')"
 missing="$(grep -o "404" <<< "$response_stripped")"
 if [ "$missing" == 404 ]
 then
-echo -e ""$tag" is missing!" >> /tmp/artifacts/missing.txt
-echo -e "$tag is missing!"
+echo -e ""$tag" is missing!" | tee -a /tmp/missing.txt
 else
 echo "lcd soundsystem - we still got it"
 fi  
